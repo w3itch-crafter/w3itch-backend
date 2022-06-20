88665c1ee2af8198c8a9a41fc777f92334e7eea9
xiaodao <lychees67@gmail.com>
2022-06-20 22:13:31 +0800

    fix: typo

--- src/migrations/1655646827134-AlterTableUserAvatar.ts
+++ src/migrations/1655646827134-AlterTableUserAvatar.ts
@@ -4,7 +4,7 @@ export class AlterTableUserAvatar1655646827134 implements MigrationInterface {
     name = 'AlterTableUserAvatar1655646827134'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`avatar\` \`avatar\` varchar(255) NOT NULL DEFAULT 'https://image.w3itch.io/w3itch-test/attachment/5/c388baa8-c244-4782-9807-978a8dcb7700.pngg'`);
+        await queryRunner.query(`ALTER TABLE \`user\` CHANGE \`avatar\` \`avatar\` varchar(255) NOT NULL DEFAULT 'https://image.w3itch.io/w3itch-test/attachment/5/c388baa8-c244-4782-9807-978a8dcb7700.png'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
